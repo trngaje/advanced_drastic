e1b9ec5845aadc99dce3bb674a94150864daad36
trngaje <bilbobaggins@naver.com>
2026-06-18 21:13:28 +0900

    1.add drastic binaries and script 2.remove unused .cf2 files and bios

--- launch.sh
+++ launch.sh
@@ -6,5 +6,10 @@ export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
 
 cd $mydir
 
-./drastic "$1"
+CURRENT_DIR=`pwd`
+
+unset LD_PRELOAD
+export LD_PRELOAD=$CURRENT_DIR/libs/libadvdrastic.so
+
+./drastic_v2522 "$1"
 
